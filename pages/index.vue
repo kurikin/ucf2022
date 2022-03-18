a80e989a46db8bf1bc089b8e0771f05122a1838d
kurikin <riku.code0309@gmail.com>
2022-03-18 13:27:42 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- ヘッダー -->
+    <header class="header">
+      <img class="logo" src="logo.png" alt="" />
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+
+  width: 100%;
+  background: url('/header-back.jpg');
+  background-position: center;
+
+  .logo {
+    width: 100%;
+    height: 100%;
+    grid-column: 1;
+    grid-row: 1 / -1;
+  }
+}
+</style>
