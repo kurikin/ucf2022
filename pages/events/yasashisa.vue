375391eac8d88e69f70bc39264c9b06175bc4c41
kurikin <riku.code0309@gmail.com>
2022-05-25 17:11:24 +0900

    Change department image for kiban

---
+++ pages/events/yasashisa.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <WhiteHeader>
+      <template v-slot:page-title>優<span>し</span>さの街</template>
+    </WhiteHeader>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
