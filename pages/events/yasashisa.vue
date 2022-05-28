ef05fcebd883ad7fdf02bd11c1b3bd65c9f59fdc
kurikin <riku.code0309@gmail.com>
2022-05-28 13:35:54 +0900

    Add description to yasashisa page

--- pages/events/yasashisa.vue
+++ pages/events/yasashisa.vue
@@ -17,6 +17,37 @@
             :resize="true"
           ></youtube>
         </div>
+        <section class="about">
+          <div class="heading-box">
+            <h2 class="heading">「優しさの街」について</h2>
+            <div class="sns fadeIn animation-2">
+              <a
+                class="sns-link"
+                href="https://twitter.com/cus_ucf_oc"
+                target="_blank"
+              >
+                <img class="sns-logo" src="/logos/twitter-brown.svg" alt="" />
+              </a>
+              <a
+                class="sns-link"
+                href="https://www.instagram.com/cus_ucf_oc/?hl=ja"
+                target="_blank"
+              >
+                <img class="sns-logo" src="/logos/facebook-brown.svg" alt="" />
+              </a>
+              <a
+                class="sns-link"
+                href="https://note.com/ucf2022/"
+                target="_blank"
+              >
+                <img class="sns-logo" src="/logos/note-brown.svg" alt="" />
+              </a>
+            </div>
+          </div>
+          <p class="description">
+            「優しさの街」は、都市基盤学科の有志学生による団体です。SNSでの土木紹介やオープンキャンパスの実施を通して、普段土木に馴染みがない人にも土木の魅力を知ってもらいたい！そんな思いで活動しています。さらに、中高生に学生生活の様子を感じてもらいたい！との願いを実現すべく、現役の大学生の私たちだからこそできるフレッシュな発信を心がけています。
+          </p>
+        </section>
       </div>
     </div>
   </div>
@@ -47,6 +78,7 @@ export default {
 .youtube-wrapper {
   max-width: 800px;
   margin: 0 auto;
+  margin-bottom: 24px;
 }
 
 .youtube {
@@ -55,5 +87,41 @@ export default {
   height: 100%;
   top: 0;
   left: 0;
+}
+
+.heading-box {
+  display: flex;
+  flex-direction: column-reverse;
+  gap: 12px;
+  margin-bottom: 16px;
+
+  .heading {
+    font-size: 24px;
+    color: $black;
+  }
+}
+
+.sns {
+  display: flex;
+  gap: 32px;
+  grid-row: 1;
+  grid-column: 2;
+  justify-self: end;
+  align-self: start;
+
+  .sns-link {
+    width: 24px;
+    height: 24px;
+  }
+
+  .sns-logo {
+    width: 100%;
+    height: 100%;
+  }
+}
+
+.description {
+  font-size: 16px;
+  color: $black;
 }
 </style>
