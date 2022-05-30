6b60cb174364857ebfb3202dbfc770262022e8bb
kurikin <riku.code0309@gmail.com>
2022-05-31 01:05:10 +0900

    Move ogp image to logos folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: 'https://ucf2022.com/ogp.png',
+        content: 'https://ucf2022.com/logos/ogp.png',
       },
       { name: 'twitter:card', content: 'summary_large_image' },
     ],
