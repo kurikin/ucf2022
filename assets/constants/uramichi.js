68242f6f5f6f3a4d2ad8c843c77a1e700a04c0f3
kurikin <riku.code0309@gmail.com>
2022-06-19 16:14:43 +0900

    Add youtube links of uramichi to uramichi.js

---
+++ assets/constants/uramichi.js
@@ -0,0 +1,42 @@
+export const uramichi = [
+  {
+    url: 'https://youtu.be/4MJ-818yNsw',
+    placeName: '第一研究棟前',
+  },
+  {
+    url: 'https://youtu.be/ayCmJlU6FJ4',
+    placeName: '都市科学部講義棟裏',
+  },
+  {
+    url: 'https://youtu.be/EcyuCSWI_UE',
+    placeName: '総合研究棟付近',
+  },
+  {
+    url: 'https://youtu.be/V-CY8UeelqM',
+    placeName: '第１食堂付近',
+  },
+  {
+    url: 'https://youtu.be/JPI6-nOzJZ4',
+    placeName: '第１食堂裏',
+  },
+  {
+    url: 'https://youtu.be/cRqR3Fdu_kk',
+    placeName: '第１食堂下',
+  },
+  {
+    url: 'https://youtu.be/892fMmfyPMQ',
+    placeName: '経済学部講義棟エリア',
+  },
+  {
+    url: 'https://youtu.be/rXSLkQngoFQ',
+    placeName: '環境情報4号棟付近',
+  },
+  {
+    url: 'https://youtu.be/OaiGrPbKtIE',
+    placeName: '正門横から大学会館',
+  },
+  {
+    url: 'https://youtu.be/IkzC4zv3DgE',
+    placeName: '北門付近の外周道路',
+  },
+]
